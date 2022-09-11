8a603c131bca3a3d5c01e42cea35e41e62d988a6
aamersohailgit <84438123+aamersohailgit@users.noreply.github.com>
2022-09-11 20:04:46 +1000

    checkingg code

---
+++ app/app/tests.py
@@ -0,0 +1,19 @@
+"""Sample testes"""
+
+from django.test import SimpleTestCase
+
+from app.calc import add, sub
+
+
+class CalcTests(SimpleTestCase):
+    """Test the calc module"""
+
+    def test_add_numbers(self):
+        res = add(5, 6)
+
+        self.assertEqual(res, 11)
+
+    def test_subtract_numbers(self):
+        res = sub(10, 5)
+
+        self.assertEqual(res, 5)
